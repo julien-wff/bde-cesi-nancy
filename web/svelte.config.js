63a49170c35198c865d44d9727724444f2f0ec15
Julien W <cefadrom1@gmail.com>
2025-11-23 14:19:24 +0100

    feat: set build mode to static

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-static';
 import autoprefixer from 'autoprefixer';
 import preprocess from 'svelte-preprocess';
 
@@ -13,7 +13,10 @@ const config = {
     }),
 
     kit: {
-        adapter: adapter()
+        adapter: adapter(),
+        prerender: {
+            handleHttpError: 'warn',
+        },
     }
 };
 
